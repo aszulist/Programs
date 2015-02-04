ee905bc075ca58b93b5ba961f002e43dcb4f7a3f
Setzo <setzo94@gmail.com>
2015-02-04 19:37:39 +0100

    invokeUpgrd

--- src/invoke/Invoke.java
+++ src/invoke/Invoke.java
@@ -6,18 +6,51 @@ import java.util.Scanner;
 public class Invoke {
 
 	public static void main(String[] args) {
-		
-		Scanner data = new Scanner(System.in);
-		LinkedList<Person> people = new LinkedList<Person>();
-		
-		for(int i=0; i<3; i++){
-			Person guy = new Person();
-			System.out.println("Podaj imie");
-			people.add(guy);
-			
-			guy.setName(data.nextLine());
-			
-		}
 
+		Scanner data = new Scanner(System.in);
+		
+		LinkedList<Person> people = new LinkedList<Person>();
+
+		for (int i = 0; i < 3; i++) {
+
+			Person guy = new Person();
+
+			System.out.println("Podaj imie:");
+			guy.setName(data.nextLine());
+
+			System.out.println("Podaj nazwisko:");
+			guy.setSurname(data.nextLine());
+			
+			System.out.println("Podaj wiek(int):");
+			guy.setAge(data.nextInt());
+			
+			System.out.println("Podaj zatrudnienie (0-2 int):");
+			int empCat = data.nextInt();
+			switch(empCat) {
+			case 0 : {
+				guy.setEmployment(EmpCat.EMPLOYED);
+				break;
+			}
+			case 1 : {
+				guy.setEmployment(EmpCat.SELF_EMPLOYED);
+				break;
+			}
+			case 2 : {
+				guy.setEmployment(EmpCat.UNEMPLOYED);
+				break;
+			}
+			default : {
+				guy.setEmployment(EmpCat.UNEMPLOYED);
+				break;
+			}
+			}
+			
+			System.out.println("Podaj nr telefonu:");
+			guy.setPhoneNumber(data.nextLine());
+			
+			people.add(guy);
+		}
+		
+		data.close();
 	}
 }
