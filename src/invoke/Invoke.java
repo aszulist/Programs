1901cf3a816b7f67c1858233c4244e6882ea8418
unknown <badamziom@o2.pl>
2015-02-04 19:12:17 +0100

    Start

--- src/invoke/Invoke.java
+++ src/invoke/Invoke.java
@@ -1,10 +1,18 @@
 package invoke;
 
+import java.util.Scanner;
+
 public class Invoke {
 
 	public static void main(String[] args) {
 		
+		Scanner data = new Scanner(System.in);
+		LinkedList<Person> people = new LinkedList<Person>();
 		
+		
+		System.out.println("Podaj liczbe");
+		
+		Sy
 
 	}
 }
